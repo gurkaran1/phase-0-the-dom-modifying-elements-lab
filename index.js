24fa344828849efee6f4f2ae40d9961ab924d835
Gurkaran Singh <sgurkaran24@gmail.com>
2023-03-17 17:39:49 -0400

    final

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+const newHeader = document.createElement("h1");
+newHeader.textContent = "Kartoos is the champion";
+newHeader.setAttribute("id", "victory");
+const mainElement = document.querySelector("#main");
+mainElement.parentNode.removeChild(mainElement);
